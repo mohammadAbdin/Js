22aa4fe618fb4f1a9157fced0bcb862a01a57d7f
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-04-14 13:45:27 +0300

    let, const, var

---
+++ day_1/7 - Understanding let, const, and var.md/index.js
@@ -0,0 +1,33 @@
+// 1-
+var number = 1;
+
+// 2-
+let string = "mohammad";
+
+// 3-
+const boolean = true;
+
+// 4-
+var number1 = 1;
+
+console.log(number1);
+number1 = 3;
+
+console.log(number1);
+// 5-
+let string1 = "mohammad";
+console.log(string1);
+
+string1 = "Abdin";
+console.log(string1);
+// 6-
+const boolean1 = true;
+console.log(boolean1);
+boolean1 = false;
+console.log(boolean1);
+
+//The Error:: Uncaught TypeError TypeError: Assignment to constant variable.
+
+// Because we cannot reassign a const variable
+
+// Because it is unchangable
