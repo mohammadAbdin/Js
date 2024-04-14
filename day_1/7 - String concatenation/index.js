712642e39c042b768ba3e4868803c0c1b0e06fbb
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-04-14 18:41:16 +0300

    string concat

---
+++ day_1/7 - String concatenation/index.js
@@ -0,0 +1,25 @@
+// 1-
+const name = "mohammad";
+
+const age = "22";
+
+console.log("My name is " + name + " and i am " + age + " years old");
+
+// 2-
+const favColor = "black";
+const favMovie = "johnWick";
+const favFood = "mansaf";
+
+console.log(
+  "My favorite color is " +
+    favColor +
+    " and my favorite Movie is " +
+    favMovie +
+    " and my favorite food is " +
+    favFood
+);
+
+// 3-
+const today = "Wendesday";
+
+console.log("Today is " + today);
