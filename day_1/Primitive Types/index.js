b71cecb10c8461a19084883bb9708f8a67069370
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-04-14 12:55:09 +0300

    varibales

---
+++ day_1/Primitive Types/index.js
@@ -0,0 +1,15 @@
+const string = "mohammad";
+
+const number1 = 3;
+
+const number2 = 6;
+
+console.log(number1 + number2);
+
+const boolean = ture;
+
+const variable = undefined;
+
+console.log(variable);
+
+const var2 = null;
