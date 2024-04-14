4f01f46b026c5e923094c92633eac70659cc8331
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-04-14 16:05:40 +0300

    typeof operator

---
+++ day_1/2 - typeof Operator/index.js
@@ -0,0 +1,43 @@
+// 1-
+const num = 10;
+const str = "Hello";
+const bool = true;
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+// number
+// string
+// boolean
+
+// 2-
+
+const firstVar = 42;
+const secondVar = "42";
+
+console.log(typeof firstVar);
+console.log(typeof secondVar);
+// number
+// string
+
+// 3-
+const noValue = undefined;
+const emptyValue = "";
+
+console.log(noValue);
+console.log(emptyValue);
+// undefined
+//
+
+// 4-
+const num1 = 10;
+const str1 = "Hello";
+const boolean = true;
+console.log(`The value of num1 is ${num1} an it is type is ${typeof num1}`);
+console.log(`The value of num1 is ${str1} an it is type is ${typeof str1}`);
+console.log(
+  `The value of num1 is ${boolean} an it is type is ${typeof boolean}`
+);
+// The value of num1 is 10 an it is type is number
+// The value of num1 is Hello an it is type is string
+// The value of num1 is true an it is type is boolean
