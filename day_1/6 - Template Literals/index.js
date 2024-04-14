29f5506e0e32ee51708f4214f9ed5a755dca7f2c
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-04-14 15:42:24 +0300

    templets

---
+++ day_1/6 - Template Literals/index.js
@@ -0,0 +1,36 @@
+// 1-
+const name = "Mohammd";
+const age = 22;
+
+console.log(`My name is ${name} and i am ${age} years old ..`);
+
+// 2-
+
+const length = 5;
+const width = 3;
+const area = width * length;
+
+console.log(
+  `The area of a rectangle with Length ${length} and width of ${width} is ${area}`
+);
+// 3-
+const today = "Wednesday";
+const task1 = "program with friends";
+const task2 = "watch films";
+const task3 = "play football";
+
+console.log(`on ${today} i want to ${task1}, ${task2}, ${task3}`);
+
+// 4-
+const petNumber = 3;
+console.log(`I have ${petNumber} pets`);
+
+// 5-
+
+const height = 100;
+const base = 100;
+const triangleArea = (base * height) / 2;
+
+console.log(
+  `The area of a triangle iwth base ${base} and height ${height} is ${triangleArea}`
+);
