fc03ac7719c3bf1ebb85ed030e0259bd188ceb2b
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-05-15 22:19:45 +0300

    Exercise 6 - Table Manipulation.pdf

---
+++ day_12/Exercise 5 - Paragraph Highlighter.pdf copy/script.js
@@ -0,0 +1,22 @@
+function toggleColumn(index) {
+  const rows = document.querySelectorAll("#dataTable tr");
+
+  rows.forEach((row) => {
+    const cell = row.cells[index];
+
+    if (cell.style.display === "none") {
+      cell.style.display = "";
+    } else {
+      cell.style.display = "none";
+    }
+  });
+}
+
+const resetTable = () => {
+  const rows = document.querySelectorAll("#dataTable tr");
+  rows.forEach((row) => {
+    Array.from(row.cells).forEach((cell) => {
+      cell.style.display = "";
+    });
+  });
+};
