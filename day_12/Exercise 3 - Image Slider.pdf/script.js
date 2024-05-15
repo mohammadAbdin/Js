90a275901c2d8bb0d225e2bc61ead89722185074
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-05-15 13:40:52 +0300

    Exercise 3 - Image Slider.pdf

---
+++ day_12/Exercise 3 - Image Slider.pdf/script.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll("img");
+let currentIndex = 0;
+
+const moveSlide = (direction) => {
+  slides[currentIndex].classList.remove("active");
+
+  currentIndex += direction;
+
+  if (currentIndex < 0) {
+    currentIndex = 0;
+  } else if (currentIndex >= slides.length) {
+    currentIndex = slides.length - 1;
+  }
+
+  slides[currentIndex].classList.add("active");
+};
